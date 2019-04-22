b520c8f4196bbdb466323e5768dbce11eed8444b
ayzaki93 <46251022+ayzaki93@users.noreply.github.com>
2019-04-22 16:30:12 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -13,7 +13,7 @@ selfbot.on('ready', () => {
 selfbot.on("ready", () => {
     selfbot.user.setPresence({
         game: { 
-            name: "Project Espada 🌙",
+            name: "Project WeeKy 🌙",
             type: 'STREAMING',
             url: 'https://www.twitch.tv/something'
         }
