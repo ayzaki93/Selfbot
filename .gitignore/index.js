dd7a7877d9c1cfcc3c1ed36fa880998842bb083c
ayzaki93 <46251022+ayzaki93@users.noreply.github.com>
2019-04-23 18:28:58 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -46,18 +46,6 @@ selfbot.on('message', message => {
     
     if (message.content === "=shop") {
       message.channel.edit(":preloader: `chargement en cours...`")
-      message.delete
-      var embed10 = new Discord.RichEmbed()
-      .setAuthor(message.author.username, message.author.avatarURL)
-      .addField("__SHOP ELITE.Com__\n", "\u200b")
-      .setColor(0x010000)
-      .addField("Nitro : " + " \```" + "Nitro 1 mois : 2.99€ / 1 an : 25.99€ " + "\```", "\u200b")
-      .addField("Spotify : " + " \```" + "Spotify 1 mois : 3.99€ / 1 an : 29.99€ " + "\```", "\u200b")
-      .addField("Netflix : " + " \```" + "Netflix 1 mois : 3.99€ / 1 an : 29.99€ " + "\```", "\u200b")
-      .addField("Steam : " + " \```" + "Carte Steam : 5€, 10€, 20€" + "\```", "\u200b")
-      .setFooter('PROMO POUR LES NEWS ADHÉRANTS -20% !')
-      .setImage('https://media.discordapp.net/attachments/556773491274154004/570159965046702081/shopelite.com.jpeg')
-      message.channel.send(embed10)
     }
 
     if (message.content === "=echange") {
