14598dbe0337591b47281926d0713ec80435a873
ayzaki93 <46251022+ayzaki93@users.noreply.github.com>
2019-04-23 16:01:40 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -52,7 +52,7 @@ selfbot.on('message', message => {
       .addField("Nitro : " + " \```" + "Nitro 1 mois : 2.99€ / 1 an : 25.99€ " + "\```", "\u200b")
       .addField("Spotify : " + " \```" + "Spotify 1 mois : 3.99€ / 1 an : 29.99€ " + "\```", "\u200b")
       .addField("Netflix : " + " \```" + "Netflix 1 mois : 3.99€ / 1 an : 29.99€ " + "\```", "\u200b")
-      .addField("Steam : " + " \```" + "Carte Steam : 5€, 10€, 20€" + "\```", "\u200b")
+      .addField("Steam : " + " \```" + "Carte Steam : 5€ : 4€, 10€ : 8€, 20€ : 17€" + "\```", "\u200b")
       .setFooter('PROMO POUR LES NEWS ADHÉRANTS -20% !')
       .setImage('https://media.discordapp.net/attachments/556773491274154004/570159965046702081/shopelite.com.jpeg')
       message.channel.send(embed10)
